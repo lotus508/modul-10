92ace1746089d6a1e5a6d702bbc37766f88011e4
Жас <zhasulan@bk.ru>
2022-04-03 16:41:59 +0600

    modul-10

--- cart.js
+++ cart.js
@@ -5,9 +5,9 @@
     totalPrice: 0,  //- общая стоимость корзины
     count: 0,       //- количество товаров
 
-    getTotalPrice() { 
-      return this.totalPrice;
-    },
+    //getTotalPrice() { 
+     // return this.totalPrice;
+    //},
     add(ProductName, ProductPrice, ProductCount = 1) {
       const Product = {
         'Продукт': ProductName,
@@ -16,13 +16,14 @@
       };
       this.items.push(Product);
       this.increaseCount(ProductCount);
-      this.calculateItemPrice();
+      //this.calculateItemPrice();
     },
     increaseCount(ProductCount) {
       this.count += ProductCount;
     },
     calculateItemPrice() {
-        this.totalPrice = this.items.reduce(
+      //this.totalPrice = this.items.reduce(
+      return this.items.reduce(
         (total, item) => total + item.ProductPrice * item.ProductCount, 0
       );
     },
@@ -35,10 +36,18 @@
       console.log(`${JSON.stringify(this.items)}\n стоимость корзины: ${this.totalPrice}`);
     },
   };
+//Для добавления геттера к существующему объекту в любое время используйте Object.defineProperty()
+  Object.defineProperty(cart, 'totalPrice', {
+    get() {
+      return this.calculateItemPrice();
+    },
+  });
 
     cart.add("Лук", 720, 1);
     cart.add("Картофель", 320, 7);
     cart.add("Апельсин", 880, 2);
     cart.add("Курица", 2200, 3);
     cart.add("Сахар", 375, 4);
+    //cart.totalPrice = 10;
+
     cart.print();
